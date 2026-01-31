5d5f6a51504c5c5a612dfb69998b597dbfa39007
Krzysztof <ksieja@student.agh.edu.pl>
2026-01-31 10:29:27 +0100

    pierwszy commit

---
+++ src/main/java/pl/edu/agh/hangman/GenerateWord.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.hangman;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class GenerateWord {
+    public static String generateWord() {
+        String word;
+        ArrayList<String> words = new ArrayList<>();
+        String sciezkapliku="resources/slowa.txt";
+
+
+        word = "test";
+        return word;
+    }
+}
---
+++ src/main/java/pl/edu/agh/hangman/testowo.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.hangman;
+
+public class testowo {
+    public static void main(String[] args) {
+        GenerateWord generateWord = new GenerateWord();
+        System.out.println(generateWord.generateWord());
+
+    }
+}
