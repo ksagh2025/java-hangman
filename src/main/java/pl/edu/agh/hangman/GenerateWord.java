178f82c16b3842d8b86802dde8aeee226c929ae4
Krzysztof <ksieja@student.agh.edu.pl>
2026-01-31 10:56:34 +0100

    GenerateWord gotowe !

--- src/main/java/pl/edu/agh/hangman/GenerateWord.java
+++ src/main/java/pl/edu/agh/hangman/GenerateWord.java
@@ -4,23 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GenerateWord {
     public static String generateWord() {
         String word;
+        int losowa;
         ArrayList<String> words = new ArrayList<>();
-        String sciezkapliku="resources/slowa.txt";
+        String sciezkapliku="src/main/resources/slowa.txt";
+        Path path = Paths.get(sciezkapliku);
 
-        try (BufferedReader reader = Files.newBufferedReader(sci.toPath())) {
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
             String line = null;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                //System.out.println(line);
+                words.add(line);
             }
         } catch (IOException x) {
             System.err.format("IOException: %s", x);}
 
+        Random generator = new Random();
+        losowa = generator.nextInt(words.size());
         word = "test";
-        return word;
+        return words.get(losowa);
     }
 }
